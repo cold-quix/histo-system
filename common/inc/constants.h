d1f1d09a2df36ed91c5399c19428ddf6b591f6de
jlazarskiparkin9815 <jlazarskiparkin9815@conestogac.on.ca>
2020-05-02 23:04:21 -0400

    Added some universal constants to common/inc/constants.h

--- common/inc/constants.h
+++ common/inc/constants.h
@@ -3,9 +3,23 @@ NAME:			constants.h
 DATE:			2020-04-18
 CODER:			Jack Lazarski Parkinson
 DESCRIPTION:	
-	This file contains global constants used in the HISTO-SYSTEM system.
+	This file contains global constants used in the HISTO-SYSTEM system
+	and #include statements for code common to all three programs.
 	
 */
+
+// Debug toggle
+#define DEBUG			0 // Value doesn't matter
+
+
+// Constants used by all programs
+#define SHM_START		0
+#define SHM_END			255
+#define SHM_SIZE		256
+#define SHM_INVALID		-1
+
+// Include statments for all programs
+#include <stdio.h>
 
 
 
