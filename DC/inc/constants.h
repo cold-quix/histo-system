481312ae3ca9176852e9e00b6d3e23a568e997eb
jlazarskiparkin9815 <jlazarskiparkin9815@conestogac.on.ca>
2020-05-07 16:34:54 -0400

    Added DC constants.h file.

---
+++ DC/inc/constants.h
@@ -0,0 +1,60 @@
+/*
+NAME:			constants.h
+DATE:			2020-04-18
+CODER:			Jack Lazarski Parkinson
+DESCRIPTION:	
+	Constants used by the DC program to track characters written into 
+	shared memory by DP-1 and DP-2.
+*/
+
+//
+#define MULTIPLE_10_SEC		5
+
+// The indices in the array where the count of each character is stored.
+#define DC_ARRAY_LENGTH		19
+#define DC_INDEX_A			0
+#define DC_INDEX_C			1
+#define DC_INDEX_D			2
+#define DC_INDEX_E			3
+#define DC_INDEX_F			4
+#define DC_INDEX_G			5
+#define DC_INDEX_H			6
+#define DC_INDEX_I			7
+#define DC_INDEX_J			8
+#define DC_INDEX_K			9
+#define DC_INDEX_L			10
+#define DC_INDEX_M			11
+#define DC_INDEX_N			12
+#define DC_INDEX_O			13
+#define DC_INDEX_P			14
+#define DC_INDEX_Q			15
+#define DC_INDEX_R			16
+#define DC_INDEX_S			17
+#define DC_INDEX_T			18
+
+// The value of each potential character as an int.
+#define DC_VALUE_A			65
+#define DC_VALUE_B			66
+#define DC_VALUE_C			67
+#define DC_VALUE_D			68
+#define DC_VALUE_E			69
+#define DC_VALUE_F			70
+#define DC_VALUE_G			71
+#define DC_VALUE_H			72
+#define DC_VALUE_I			73
+#define DC_VALUE_J			74
+#define DC_VALUE_K			75
+#define DC_VALUE_L			76
+#define DC_VALUE_M			77
+#define DC_VALUE_N			78
+#define DC_VALUE_O			79
+#define DC_VALUE_P			80
+#define DC_VALUE_Q			81
+#define DC_VALUE_R			82
+#define DC_VALUE_S			82
+#define DC_VALUE_T			84
+
+
+
+
+
